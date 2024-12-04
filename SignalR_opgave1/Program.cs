bbc5abea1d7270b171050b590755747cd8668bb6
Daniel Bonde Rasmussen <15269@student.eadania.dk>
2024-12-04 11:00:41 +0100

    Add project files.

---
+++ SignalR_opgave1/Program.cs
@@ -0,0 +1,14 @@
+using SignalR_opgave1;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+//app.MapGet("/", () => "Hello World!");
+app.MapHub<ProductHub>("ProductHub");
+app.Run();
