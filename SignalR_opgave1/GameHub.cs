c95c38cf83202c67a8715bb6858909ebce7cea8d
Daniel Bonde Rasmussen <15269@student.eadania.dk>
2024-12-16 13:26:19 +0100

    Adds working game

--- SignalR_opgave1/GameHub.cs
+++ SignalR_opgave1/GameHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Drawing;
 
 namespace SignalR_opgave1
 {
@@ -6,22 +7,19 @@ namespace SignalR_opgave1
     {
         private static readonly int GridSize = 5;
         private static readonly string[] Grid = new string[GridSize * GridSize];
-        private static readonly Dictionary<string, string> PlayerColors = new()
-        {
-            { "Player1", "red" },
-            { "Player2", "blue" }
-        };
+        private static string Id1 = "";
+        private static string Id2 = ""; 
 
         public override async Task OnConnectedAsync()
         {
-            // Assign players to Player1 or Player2
-            if (!PlayerColors.ContainsKey(Context.ConnectionId))
+            if(Id1 == "")
             {
-                var playerCount = PlayerColors.Count;
-                var playerKey = playerCount == 0 ? "Player1" : "Player2";
-                PlayerColors[Context.ConnectionId] = PlayerColors[playerKey];
+                Id1 = Context.ConnectionId;
             }
-
+            else if(Id2 == "") 
+            {
+                Id2 = Context.ConnectionId;
+            }
             // Send the current grid state to the new client
             await Clients.Caller.SendAsync("UpdateGrid", Grid);
             await base.OnConnectedAsync();
@@ -29,17 +27,22 @@ namespace SignalR_opgave1
 
         public async Task ClickCell(int i)
         {
-            Console.WriteLine("ClickCell called.");
+            if(Id1 == Context.ConnectionId)
+            {
+                Console.WriteLine("");
+            }
+            else if(Id2 == Context.ConnectionId)
+            {
+                Console.WriteLine("");
+            }
 
             if (i<0 || i>= GridSize*GridSize)
                 return;
 
             // Update grid with player's color
-            if (PlayerColors.TryGetValue(Context.ConnectionId, out var color))
-            {
-                Grid[i] = color;
+            Grid[i] = "Red";
                 await Clients.All.SendAsync("UpdateGrid", Grid);
-            }
+            
         }
 
         public async Task ResetGrid()
